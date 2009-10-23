584f1646f6541e5c4a80119c292f565c79763d96
kkansky <kkansky@5faf4222-bf6a-11de-8ec6-755e00613368>
2009-10-23 04:19:54 +0000

    SQL db working

--- models/course.php
+++ models/course.php
@@ -3,6 +3,19 @@
 class Course extends Model
 {
     protected $database = 'naturesa_finna';
+
+
+    public function getCourses()
+    {
+        return $this->query("SELECT * FROM courses");
+    }
+
+    
+    public function addCourse($number, $title)
+    {
+        return $this->query("INSERT INTO courses (number, title) VALUES(:number, :title)", array(':number' => $number,':title' => $title));
+    }
+    
 }
 
 ?>
