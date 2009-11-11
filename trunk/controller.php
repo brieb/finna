e60e96cc265a5801ea97316b9074ef678be4ecc2
kkansky <kkansky@5faf4222-bf6a-11de-8ec6-755e00613368>
2009-11-11 22:29:12 +0000

    courses list populated

--- trunk/controller.php
+++ trunk/controller.php
@@ -2,7 +2,7 @@
 
 class AppController
 {
-    public $uses = array('Course');
+    public $uses = array('Course','User','Assignment');
     public $config = null;
     /*
      *   Load the models
@@ -14,13 +14,11 @@ class AppController
         if (isset($this->uses)){
             foreach ($this->uses as $use){
                 $model = '../models/'.strtolower($use).'.php';
-                
                 if (!file_exists($model)) {
                     throw new Exception('Missing model: '.$model,1);
                 }
-                
                 require_once($model);
-                $this->$use = new $use();
+		$this->$use = new $use();
             }
         }
         
@@ -32,6 +30,30 @@ class AppController
     public function index()
     {
         set('page_title','First Finna page');
+	set('userCourses', $this->User->getCourses(1));
+	$assignments = $this->Assignment->getAssignments(1);
+	set('assignmentsByDue', $assignments);
+	$byPriority = Array( Array(), Array(), Array() );
+	foreach ($assignments as $assign){
+	  if ($assign['complete']==1) continue;
+	  $assign['complete'] =  0 | $assign['complete'];
+	  $assign['priority'] =  1 | $assign['priority'];
+	  $byPriority[$assign['priority']][] = $assign;
+	}
+	set('assignmentsByPriority', array_merge($byPriority[2],$byPriority[1],$byPriority[0]));
+	
+    }
+
+    public function getCourses()
+    {
+        echo json_encode( $this->Course->findCoursesByNumber($_REQUEST['search'].'%') );
+    }
+
+    public function addUserCourse()
+    {
+      $result = "success";
+      $this->User->addCourse(1, $_REQUEST['cid']);
+      echo json_encode( array("result"=>$result, "course"=>$this->Course->getCourse($_REQUEST['cid']) ) );
     }
 
     public function courses()
--- trunk/models/course.php
+++ trunk/models/course.php
@@ -4,9 +4,16 @@ class Course extends Model
 {
     protected $database = 'naturesa_finna';
 
-    public function findCoursesByNumber($search)
+    public function findCoursesByNumber($search, $limit=10)
     {
-        return $this->query("SELECT * FROM courses WHERE number LIKE '$search'");
+        return $this->query("SELECT * FROM courses WHERE number LIKE :search LIMIT $limit", array(':search'=>$search));
+    }
+
+    public function getCourse($id)
+    {
+        $results = $this->query("SELECT * FROM courses WHERE id=:id", array(':id'=>$id));
+	if (empty($results)) return null;
+        return $results[0];
     }
     
     public function addCourse($number, $title, $description)
