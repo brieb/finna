b901f8bed60853eab72e57b1586597ecf31f78ce
kkansky <kkansky@5faf4222-bf6a-11de-8ec6-755e00613368>
2009-11-18 05:55:21 +0000

    linking pages

---
+++ views/elements/prioritySort.php
@@ -0,0 +1,20 @@
+
+			    <ul>
+			        <?php foreach ($assignmentsByPriority as $assignment): ?>
+                        <li loc="assignment-info-<?= $assignment['id'] ?>" header="assignment-header" class="scrollable" style="font-family:Helvetica">
+                        	<div class="row-selection-BG">
+                        	</div>
+                            <img src="/finna/img/<?= $priorityImages[$assignment['priority']] ?>" style="position:absolute; <?= $priorityPositions[$assignment['priority']] ?>"/>
+                            <div class="li-assignment-name">
+                                <?= $assignment['title'] ?>
+                            </div>
+            				<div class="li-course-number">
+            					<?= $assignment['course_number'] ?>
+            				</div>
+            				<div class="li-due-date">
+            					Tue Apr 31
+            				</div>
+                            <div class="li-arrow-template"></div>
+                        </li>
+                    <?php endforeach; ?>
+			    </ul>
