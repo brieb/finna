6f6696c1fcbd3c183f18e630deb0d0aa4ef2d64d
livelifeinspired <livelifeinspired@5faf4222-bf6a-11de-8ec6-755e00613368>
2009-11-19 09:31:48 +0000

    date changes

--- views/templates/default.php
+++ views/templates/default.php
@@ -54,9 +54,8 @@ function cancel() {
 	/*
 var curDateDef = new Date();
 	var months = { 1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec' };
-	document.getElementById('result').innerHTML = months[curDateDef.getMonth()+1] +' '+curDateDef.getDate()+' '+curDateDef.getFullYear();
+	document.getElementById('add-assignment-date').innerHTML = curDateDef.getDay()+' '+months[curDateDef.getMonth()+1] +' '+curDateDef.getDate();
 */
-	document.getElementById('add-assignment-date').innerHTML = 'Choose date...';
 }
 
 
