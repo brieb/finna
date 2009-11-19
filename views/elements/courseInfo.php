e901f1f7ca787dc1f128342725d585c43a0391fc
livelifeinspired <livelifeinspired@5faf4222-bf6a-11de-8ec6-755e00613368>
2009-11-19 09:22:25 +0000

    css edits

--- views/elements/courseInfo.php
+++ views/elements/courseInfo.php
@@ -2,28 +2,31 @@
 
 	        <?php foreach ($userCourses as $course): ?>
 	        	<div id="course-info-<?= $course['id'] ?>" courseId="<?= $course['id'] ?>" class="hidden child-page detailContent">
-	        	     <span class="detailTitleLabel">Course:</span><span class="detailText"><?= $course['number'] ?></span><br/><br/>
-	        	     <span class="detailTitleLabel">Title:</span><span class="detailText"><?= $course['title'] ?></span><br/><br/>
-                     
-                     <ul id="tabBarCourseBottomWindow" class="roundRectangleListType">
-						<li id="descriptionButton" class="active"><a class="showArrow" loc="">Description</a></li>
-						<li id="officeHoursButton"><a class="showArrow">Office Hours</a></li>
-						<li id="handoutsButton"><a class="showArrow">Handouts</a></li>
-						<li id="announcementsButton"<a class="showArrow">Announcements</a></li>
-					</ul>
-					<div id="courseBottomWindow" style="display:none;">
-						<div id="description" style="display:none;">
-							<?= $course['description'] ?>
-						</div>
-						<div id="officeHours" style="display:none;">
-							officeHours Pane
-						</div>
-						<div id="handouts" style="display:none;">
-							handouts Pane
-						</div>
-						<div id="announcements" style="display:none;">
-							announcements Pane
-						</div>
-					</div>
+	        	     <table>
+	        	     	<tbody>
+	        	     		<tr>
+	        	     			<td><span class="detailTitleLabel">Course:</span></td>
+	        	     			<td><div class="detailText"><?= $course['number'] ?></div></td>
+	        	     		</tr>
+	        	     		<tr>
+	        	     			<td><span class="detailTitleLabel">Title:</span></td>
+	        	     			<td><div class="detailText"><?= $course['title'] ?></div></td>
+	        	     		</tr>
+	        	     	</tbody>
+	        	     </table>
+	        	     <table style="margin-top: 5px;">
+	        	     	<tbody>
+	        	     		<tr>
+	        	     			<td colspan="2"><span class="detailTitleLabel">Description:</span><br/><div style="margin-top:5px;" class="detailText"><?= $course['description'] ?></div></td>
+	        	     		</tr>
+	        	     		<tr>
+	        	     			<td><br/><span class="detailTitleLabel">Office Hours:</span></td>
+	        				</tr>
+	        	     		<tr>
+	        	     			<td><br/><span class="detailTitleLabel">Announcements:</span></td>
+	        	     		</tr>
+	        	     	</tbody>
+	        	     </table>
+	        	   
                 </div>
 	        <?php endforeach; ?>
